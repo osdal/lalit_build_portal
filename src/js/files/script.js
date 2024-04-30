f6a5fdb323c1884b917fc6f7923d22c3941324ff
Дмитрий <osdal67@gmail.com>
2024-04-30 13:05:07 +0300

    fix

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,17 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+
+// Подравнять элементыв вверхней полосе хедерапосредине
+let widthQuantityGoods = document.querySelector('.quantity-goods');
+var width = widthQuantityGoods.offsetWidth;
+
+let emptyElement = document.querySelector('.empty');
+emptyElement.style.width = width/4 + 'px';
+
+let favorites = document.querySelector('header .favorites-block');
+let favoritesWidth = favorites.offsetWidth;
+console.log("Ширина элемента: " + favoritesWidth + " пикселей.");
+let widthSearch = document.querySelector('header .search');
+widthSearch.style.width = widthSearch + 'px';
